e312f2693f7c335b3e1b425664f666fefacc9f0f
Dorian Zedler <dorian@itsblue.de>
2021-01-21 08:54:27 +0100

    new Test :)

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 mkdir archives
 touch archives/test
 echo "This is some content" >> archives/test
+touch archives/test1
+echo "This is some content1" >> archives/test1
 cd archives
 zip -r ./archives.zip *
